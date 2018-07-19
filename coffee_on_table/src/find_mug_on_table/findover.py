09b28494bb5a68bf41f01e335a4c70baf0927a02
JRauer <johannes.rauer@hotmail.com>
2018-07-19 11:56:13 +0200

    Verallgemeinerungen: Zu suchende Objekte können über Parameter übergeben werden. Erkennung auch möglich, wenn Mittelpunkt von innerer Bounding Box in äußerer ist. Findover.py generell aufgeräumt und dokumentiert

--- coffee_on_table/src/find_mug_on_table/findover.py
+++ coffee_on_table/src/find_mug_on_table/findover.py
@@ -12,7 +12,7 @@ import cv2
 import roslib
 import rospy
 
-#cv_bridge
+# cv_bridge - needed to convert between ROS Image Msg and OpenCV cv::Mat
 from cv_bridge import CvBridge, CvBridgeError
 
 # Ros Messages
@@ -21,99 +21,118 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from darknet_ros_msgs.msg import BoundingBoxes
 
-#instantiate
-bridge = CvBridge()
-VERBOSE=False
+''' Class to subscribe to
+		- Output-Image from YOLOnet with bounding boxes
+		- Output-Array of bounding boxes (Positions of edges)
+		- Kamera-Image where the depth-image is aligned to the color-image
+	Calculates the position of the center of the innerClass in the outerClass(e.g. center of cup on table)'''
 class rossinator:
-	mug_pub = rospy.Publisher("/roi",CompressedImage, queue_size=1)
+	# Initialize CVBridge
+	bridge = CvBridge()
+	# Initialize class-variable to store bounding boxes
 	currentBoundingBoxes = BoundingBoxes()
-	#encoded_img
+	outerClass = sys.argv[1]
+	innerClass = sys.argv[2]
+	strictness = sys.argv[3]
 
 	def __init__(self):
-		self.bridge = CvBridge()
-		rospy.Subscriber("/darknet_ros/detection_image", Image, self.image_callback, queue_size=1)
-		rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, self.bounding_callback, queue_size=1)
-		rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.depth_callback, queue_size=1)
+		# Initialize Publisher and Subscribers
+		mypub = rospy.Publisher("/roi",CompressedImage, queue_size=1)	#TODO Not used yet
+		rospy.Subscriber("/darknet_ros/detection_image", Image, self.image_callback, queue_size=1)				# YOLOnet Output-Image
+		rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, self.bounding_callback, queue_size=1)	# Bounding-Box-Array
+		rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.depth_callback, queue_size=1)	# Depth-Image aligned to Color-Image
 
-	def depth_callback(self,data):
+	# Check if there is a innerClass in an outerClass
+	def image_callback(self, data):
+		'''try:
+			# Convert from ROS-Image to CV2::Mat
+			cv_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+		except CvBridgeError as e:
+			print(e)
+		windowName = "Image from YOLOnet"
+		cv2.namedWindow(windowName)
+		cv2.moveWindow(windowName, 800, 500)
+		cv2.imshow(windowName, cv_image)
+		cv2.waitKey(1)'''
+
+		# Check if there is a inner class in an outer class in the image and display message
+		if self.inner_in_outer():
+			print('Found ' + self.innerClass + ' ' + self.strictness + ' ' + self.outerClass)
+
+	# Write bounding boxes to class-variable
+	def bounding_callback(self, data):
+		self.currentBoundingBoxes = data
+
+	# Calculate depth-image
+	def depth_callback(self, data):
 		try:
-			cv_image = self.bridge.imgmsg_to_cv2(data,"32FC1")
+			# Convert from ROS-Image to CV2::Mat
+			self.cv_image = self.bridge.imgmsg_to_cv2(data,"32FC1")
+			# Convert from CV2::Mat to Array with depth-Values
+			self.depth_array = np.array(self.cv_image, dtype=np.float32)
 		except CvBridgeError as e:
 			print(e)
 
-		self.depth_array = np.array(cv_image, dtype=np.float32)
-		
-
-		#cv2.normalize(self.depth_array,self.depth_array,0,1,cv2.NORM_MINMAX)	
-		
-		#cv2.imshow("my first image", self.depth_array)
-		#cv2.waitKey(1)
-
-	def image_callback(self, data):
-		img_np_arr = np.fromstring(data.data, np.uint8)
-		self.encoded_img = cv2.imdecode(img_np_arr, cv2.IMREAD_COLOR)
-		if self.coffee_on_table():
-			print('I want myself some of that coffee sir')	    
-		#save image to some local
-
-
-	def bounding_callback(self, data):
-		self.currentBoundingBoxes = data		
-
-
-	def coffee_on_table(self):
-		#idx_table = self.find_bounding_box("person", 0)
+	def inner_in_outer(self):
+		foundInnerInOuter = False
 
 		for outer_box in self.currentBoundingBoxes.bounding_boxes:
-			if outer_box.Class == 'person':
+			if outer_box.Class == self.outerClass:
 				for inner_box in self.currentBoundingBoxes.bounding_boxes:
-					if inner_box.Class == 'cup' and inner_box.xmin > outer_box.xmin and inner_box.ymin > outer_box.ymin and inner_box.xmax < outer_box.xmax and inner_box.ymax < outer_box.ymax:
-						
-
+					if inner_box.Class == self.innerClass and self.box_is_in_box(outer_box, inner_box):
 						center_x = inner_box.xmin+(inner_box.xmax-inner_box.xmin)/2
 						center_y = inner_box.ymin+(inner_box.ymax-inner_box.ymin)/2
-						print("cup is ") 
-						print(self.depth_array[center_y][center_x])
-						print("center is: " + str(center_y) + ", " + str(center_x))
-						cv2.circle(self.depth_array,(center_x, center_y), 10, (0,0,0), -1)
+						print("center is: " + str(center_y) + ", " + str(center_x) + " Depth: " + str(self.depth_array[center_y][center_x]))
+						# Draw circle on center of bounding box of inner class
+						cv2.circle(self.cv_image,(center_x, center_y), 10, (0,0,0), -1)
+						foundInnerInOuter = True
 		
+		# Display depth-array with drawn circle
 		windowName = "Depth image with circle"
 		cv2.namedWindow(windowName)
 		cv2.moveWindow(windowName, 800, 0)
-		cv2.imshow(windowName, self.depth_array)
+		cv2.imshow(windowName, self.cv_image)
 		cv2.waitKey(1)
 
+		# Return True if found, otherwise False
+		if foundInnerInOuter == True:
+			return True
+		return False
 
-		#I return 1, if there is ore or more mugs on a table in sight
-		#and 0 otherwise
+	# Return True if inner box is in outer box (accroding to strictness), otherwise False
+	def box_is_in_box(self, outer_box, inner_box):
+		if self.strictness == 'inside':
+			if inner_box.xmin > outer_box.xmin and inner_box.xmax < outer_box.xmax and inner_box.ymin > outer_box.ymin and inner_box.ymax < outer_box.ymax:
+				return True
 
-	def find_bounding_box(self, class_name, flag):
-		probability = 0
-		size = 0
-		idx = -1
-		for index, box in enumerate(self.currentBoundingBoxes.bounding_boxes):
-			if box.Class == class_name and box.probability > probability:
-				[idx, probability] = [index, box.probability]
-				print(probability)
-				print(box.probability)
-		return idx
-		#returns index in box array with highest 
-			#0 -- probability	
-			#1 -- size	
+		if self.strictness == 'touching':
+			center_x = inner_box.xmin+(inner_box.xmax-inner_box.xmin)/2
+			center_y = inner_box.ymin+(inner_box.ymax-inner_box.ymin)/2
+			if center_x > outer_box.xmin and center_x < outer_box.xmax and center_y > outer_box.ymin and center_y < outer_box.ymax:
+				return True
+
+		if self.strictness == 'outside':
+			return True
+
+		return False
 
 def main(args):
-	'''Initializes and cleanup ros node'''
-	rospy.init_node('image_listener', anonymous=True)
-	haengst = rossinator()
+	if len(sys.argv) < 3:
+		print("  ERROR: Too few arguments given!")
+		return
+
+	#Initializes and cleanup ros node
+	rospy.init_node('cupfinder', anonymous=True)
+	cupOnTable = rossinator()
+	print('Searching for ' + cupOnTable.innerClass + ' ' + cupOnTable.strictness + ' ' + cupOnTable.outerClass)
 
 	try:
 	    rospy.spin()
 	except KeyboardInterrupt:
-		print ("Shutting down ROS Image feature detector module")
+		print ("Shutting down ROS Cupfinder")
 
+	# Close all Image-Windows 
 	cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main(sys.argv)
-
