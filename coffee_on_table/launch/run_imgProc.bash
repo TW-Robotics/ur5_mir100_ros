3987576574a035d3fe8eca9f817619550c7c9a94
JRauer <johannes.rauer@hotmail.com>
2018-08-01 16:01:12 +0200

    tf_transform hinzugefügt, um Kamera vorne an Endeffektor zu hängen bzw. Transformation zu publishen. Genau Parameter zu camera_link müssen noch ausgemessen werden. Das Bild wird mittels image_rotate rotiert um es 'richtig herum' in YOLOnet geben und anzeigen lassen zu können.

--- coffee_on_table/launch/run_imgProc.bash
+++ coffee_on_table/launch/run_imgProc.bash
@@ -5,9 +5,9 @@ xterm -e "roscore" &
 sleep 2
 #xterm -hold -e "roslaunch src/yolotest/usbcam.launch" &
 xterm -hold -e "roslaunch coffee_on_table bringUp_camera.launch" &
-#xterm -hold -e "roslaunch realsense2_camera rs_aligned_depth.launch" &
+xterm -hold -e "roslaunch coffee_on_table img_rotate.launch" &
 xterm -hold -e "roslaunch coffee_on_table yolotest.launch" &
-xterm -geometry 96x24+0-0 -hold -e "rosrun coffee_on_table cupfind person cup touching" &	# Arguments: OuterClass, InnerClass, Strictness (outside, touching (means center of inner box is in outer box), inside)
+xterm -geometry 96x24+0-0 -hold -e "rosrun coffee_on_table cupfind person cup outside" &	# Arguments: OuterClass, InnerClass, Strictness (outside, touching (means center of inner box is in outer box), inside)
 #--------------------#
 #--- We are done! ---#
 #--------------------#
