4c874cafa69110d52f859e4c60ce24542e9438a0
JRauer <johannes.rauer@hotmail.com>
2018-09-06 11:28:34 +0200

    Added sendMsg-File for testing Script-Communication on robot

---
+++ coffee_on_table/src/sendMsg.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+HOST = "192.168.0.9"    	# The remote host
+PORT = 30002              	# The same port as used by the server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send (“set_digital_out(2,True)” + “\n”)
+time.sleep (1)
+
+s.send (“popup(\”Messages\”, title=\”The Headline in the Blue box\”, blocking=True)” + “\n”)
+time.sleep (1)
+
+s.send (“set_digital_out(2,False)” + “\n”)
+time.sleep (1)
+
+data = s.recv(1024)
+
+print “Good bye!”
