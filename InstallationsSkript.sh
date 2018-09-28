586542083906065c932a8b1bb63bbb1d71ffa9e3
JRauer <johannes.rauer@hotmail.com>
2018-09-28 14:26:41 +0200

    Added pyrealsense2 python package

--- InstallationsSkript.sh
+++ InstallationsSkript.sh
@@ -61,6 +61,7 @@ sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main" -u
 sudo apt-get install librealsense2*
 ln -s "$(pwd)/realsense" ~/catkin_ws/src/realsense
+pip install pyrealsense2
 
 # PIP
 #sudo apt-get install python-pip
