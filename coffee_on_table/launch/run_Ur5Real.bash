b5971a41c0c7b5ecf16e376d79143ada9c0266ba
JRauer <johannes.rauer@hotmail.com>
2018-09-28 15:03:15 +0200

    Changed IP

--- coffee_on_table/launch/run_Ur5Real.bash
+++ coffee_on_table/launch/run_Ur5Real.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ROUTER xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.4.103" &
-xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.248" &
+xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 5
 xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true" &
 sleep 2
