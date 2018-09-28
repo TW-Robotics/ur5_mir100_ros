4bb155ab8449b2a9de7e2ca1e24162dc6604bf7c
JRauer <johannes.rauer@hotmail.com>
2018-09-28 15:03:54 +0200

    Changed IP-Adress

--- coffee_on_table/launch/run_allReal.bash
+++ coffee_on_table/launch/run_allReal.bash
@@ -9,7 +9,7 @@ xterm -hold -e "roslaunch coffee_on_table img_rotate.launch" &
 xterm -hold -e "roslaunch coffee_on_table yolotest.launch" &
 #xterm -geometry 96x24+0-0 -hold -e "rosrun coffee_on_table cupfind cup" &
 
-xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.4.103" &
+xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 5
 xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true" &
 sleep 2
