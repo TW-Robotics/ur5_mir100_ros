d3b50f431324157a54273de141b57c128eb38e21
JRauer <johannes.rauer@hotmail.com>
2018-10-26 21:15:43 +0200

    Simulation works now better but still no control of mir and a timing-bug with ur. Real robot should be controlable if timing bug does not persist

--- coffee_on_table/launch/run_real_ur5MiR.bash
+++ coffee_on_table/launch/run_real_ur5MiR.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
-xterm -hold -e "roslaunch coffee_on_table mir_ur5.launch limited:=true" &
+xterm -hold -e "roscore" &
+sleep 3
+xterm -hold -e "roslaunch coffee_on_table mir_ur5_sim.launch limited:=true" &
+sleep 2
+
+xterm -hold -e "roslaunch ur_bringup ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 5
-xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true sim:=true" &
+xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true" &
 sleep 2
 xterm -hold -e "roslaunch ur5_moveit_config moveit_rviz.launch config:=true" &
-#xterm -hold -e "roslaunch coffee_on_table tf_transform.launch" &
-#xterm -hold -e "rosrun coffee_on_table ur5_control" &
+sleep 3
+xterm -hold -e "roslaunch mir_driver mir.launch" &
 #--------------------#
 #--- We are done! ---#
 #--------------------#
