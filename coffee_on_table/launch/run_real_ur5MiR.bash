b7874eddcc8492a78e421e99deb8826cb1a7c3c9
JRauer <johannes.rauer@hotmail.com>
2018-11-02 11:20:04 +0100

    Minor changes to make mir and ur work together. Added package ur_modern_driver for compact repository. ur_modern_driver for ur-control, universal_robot for other project information

--- coffee_on_table/launch/run_real_ur5MiR.bash
+++ coffee_on_table/launch/run_real_ur5MiR.bash
@@ -4,7 +4,7 @@ sleep 3
 xterm -hold -e "roslaunch coffee_on_table mir_ur5_sim.launch limited:=true" &
 sleep 2
 
-xterm -hold -e "roslaunch ur_bringup ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
+xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 5
 xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true" &
 sleep 2
--- coffee_on_table/src/test_pub/test_pub.py
+++ coffee_on_table/src/test_pub/test_pub.py
@@ -26,15 +26,15 @@ def main(args):
 		while not rospy.is_shutdown():
 			pub.publish(myMsg1)
 			secCounter = secCounter + 1
-			if secCounter == 10:
+			if secCounter == 20:	# 30 = ca. 30 - 35 cm
 				break
 			rate.sleep()
 
 	if toDo == 2:
 		myMsg = geometry_msgs.msg.PoseStamped()
 		#myMsg.header.stamp = rospy.Time.now()	# optional
-		#myMsg.header.frame_id = '/map'	# optional
-		myMsg.pose.position.x = 13.35
+		myMsg.header.frame_id = '/map'
+		myMsg.pose.position.x = 14.35
 		myMsg.pose.position.y = 6.65
 		myMsg.pose.orientation.w = 1
 		pub1.publish(myMsg)
@@ -44,18 +44,24 @@ def main(args):
 		#myGoalMsg.header.stamp = rospy.Time.now()	# optional
 		myGoalMsg.header.frame_id = '/map' # Note: the frame_id must be map
 		#myGoalMsg.goal.target_pose.header.stamp = rospy.Time.now()	# optional
-		#myGoalMsg.goal.target_pose.header.frame_id = '/map'	# optional
+		myGoalMsg.goal.target_pose.header.frame_id = '/map'	# optional
 		myGoalMsg.goal.target_pose.pose.position.x = 13.35
 		myGoalMsg.goal.target_pose.pose.position.y = 6.65
 		myGoalMsg.goal.target_pose.pose.position.z = 0.0 # z must be 0.0 (no height in the map)
 
+		myGoalMsg.goal.target_pose.pose.orientation.w = 1
 		#myGoalMsg.goal_id.stamp = rospy.Time.now()	# optional
 		#myGoalMsg.goal_id.id = '10'	# optional
 
 		# Orientation of the robot is expressed in the yaw value of euler angles
-		angle = radians(45) # angles are expressed in radians
-		quat = quaternion_from_euler(0.0, 0.0, angle) # roll, pitch, yaw
-		myGoalMsg.goal.target_pose.pose.orientation = Quaternion(*quat.tolist())
+		#angle = radians(45) # angles are expressed in radians
+		#quat = quaternion_from_euler(0.0, 0.0, angle) # roll, pitch, yaw
+		#myGoalMsg.goal.target_pose.pose.orientation = Quaternion(*quat.tolist())
+
+		#myGoalMsg.goal.target_pose.pose.position.x = 9.15
+		#myGoalMsg.goal.target_pose.pose.position.y = 5.7
+		#myGoalMsg.goal.target_pose.pose.orientation.z = 1
+		#myGoalMsg.goal.target_pose.pose.orientation.w = 0
 
 		pub2.publish(myGoalMsg)
 
