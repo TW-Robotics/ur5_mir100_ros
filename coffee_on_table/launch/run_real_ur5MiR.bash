578c6ca02dbf63bf4a8ce9a8e37b76d6d1185613
JRauer <johannes.rauer@hotmail.com>
2018-11-16 16:20:08 +0100

    Changed tf-transforms to fit new eef-pose. Included Box and EEF in Visualisation for collission-detection. Included EEF in ur5-files for eef-point-planning

--- coffee_on_table/launch/run_real_ur5MiR.bash
+++ coffee_on_table/launch/run_real_ur5MiR.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 xterm -hold -e "roscore" &
 sleep 3
-xterm -hold -e "rosrun coffee_on_table camera_py2ros_wrapper" &
-xterm -hold -e "roslaunch coffee_on_table img_rotate.launch" &
-xterm -hold -e "roslaunch coffee_on_table yolotest.launch" &
-sleep 3
+#xterm -hold -e "rosrun coffee_on_table camera_py2ros_wrapper" &
+#xterm -hold -e "roslaunch coffee_on_table img_rotate.launch" &
+#xterm -hold -e "roslaunch coffee_on_table yolotest.launch" &
+#sleep 3
 xterm -hold -e "roslaunch coffee_on_table mir_ur5_sim.launch limited:=true" &
 sleep 2
 xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
