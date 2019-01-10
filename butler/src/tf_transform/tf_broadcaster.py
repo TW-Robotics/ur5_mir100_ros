e54431cadff9dcd05028ecdc8c69fb4d6cb45f17
JRauer <johannes.rauer@hotmail.com>
2019-01-10 20:37:59 +0100

    Doku. Deleted unused libraries. Deleted unnecessary transformations including camToBasePub

--- butler/src/tf_transform/tf_broadcaster.py
+++ butler/src/tf_transform/tf_broadcaster.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python  
-import roslib
-
 import rospy
 import tf
 import sys
-
 from geometry_msgs.msg import Pose
 
+''' Subscribe to transformation between object and camera and 
+	broadcast it to tf so other transformations can be calculated'''
+
 obj_center_pose = Pose()
-obj_center_pose.orientation.w = 1
+obj_center_pose.orientation.w = 1 	# Init as w=1 to make sure pose is valid
 
 def obj_pose_callback(data):
 	global obj_center_pose
 	obj_center_pose = data
 
 def main(args):
+	# Init Node
 	rospy.init_node('tf_broadcaster')
 
-	rospy.Subscriber("/tf_objToCam", Pose, obj_pose_callback, queue_size=1)	# Subscribe to xyz-Position of object
+	# Init subscriber to Pose of object relative to camera
+	rospy.Subscriber("/tf_objToCam", Pose, obj_pose_callback, queue_size=1)
 
+	# Init tf-broadcaster to forward pose to tf
 	br = tf.TransformBroadcaster()
 
 	# Do at a frequency of 10 Hz
 	rate = rospy.Rate(10)
-
 	while not rospy.is_shutdown():
+		# Send transformation to tf
 		br.sendTransform((obj_center_pose.position.y, -obj_center_pose.position.x, obj_center_pose.position.z),
 						 (obj_center_pose.orientation.x, obj_center_pose.orientation.y, obj_center_pose.orientation.z, obj_center_pose.orientation.w),
 						 rospy.Time.now(),
 						 "obj_center_pose",
 						 "camera_aligned_depth_to_color_frame")
-		print obj_center_pose
 		rate.sleep()
 
 if __name__ == '__main__':
--- butler/src/tf_transform/tf_listener_publisher.py
+++ butler/src/tf_transform/tf_listener_publisher.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python  
-import roslib
-
+#!/usr/bin/env python
 import rospy
-import math
 import tf
-import geometry_msgs.msg
 from geometry_msgs.msg import Pose
 import sys
 from math import pi
 
-# Convert lists to pose-obect so only 1 object has to be published
+''' Listen to transformation between object and base of robot
+	to make it turn correctly to it while searching it'''
+
+# Convert lists to pose-obect so a standard pose message can be published
 def listToPose(trans, rot):
-	pose = geometry_msgs.msg.Pose()
+	pose = Pose()
 	pose.position.x = trans[0]
 	pose.position.y = trans[1]
 	pose.position.z = trans[2]
@@ -21,18 +20,6 @@ def listToPose(trans, rot):
 	pose.orientation.w = rot[3]
 	return pose
 
-def listToPose1(trans, rot):
-	quats = tf.transformations.quaternion_from_euler(0, 0, rot[2]*pi/180, 'rxyz')
-	pose = geometry_msgs.msg.Pose()
-	pose.position.x = trans[0]
-	pose.position.y = trans[1]
-	pose.position.z = trans[2]
-	pose.orientation.x = quats[0]
-	pose.orientation.y = quats[1]
-	pose.orientation.z = quats[2]
-	pose.orientation.w = quats[3]
-	return pose
-
 def main(args):
 	# Init Node
 	rospy.init_node('tf_listener_publisher')
@@ -40,29 +27,17 @@ def main(args):
 	# Init Listener for tf-transformation
 	listener = tf.TransformListener()
 
-	# Init Publisher for camToBase Transformation
-	objToBasePub = rospy.Publisher("/tf_objToBase", Pose, queue_size=1)
-	objToWrist1Pub = rospy.Publisher("/tf_objToWrist1Pub", Pose, queue_size=1)
-	objToOriginPub = rospy.Publisher("/tf_objToOrigin", Pose, queue_size=1)
-	camToBasePub = rospy.Publisher("/tf_camToBase", Pose, queue_size=1)
-
+	# Init Publishers
+	objToBasePub = rospy.Publisher("/tf_objToBase", Pose, queue_size=1)		# For movement of R1-axis of UR when searching object
 	rospy.sleep(1)
 
 	# Do at a frequency of 10 Hz
 	rate = rospy.Rate(10.0)
 	while not rospy.is_shutdown():
 		#try:
+			# Get transformation and publish it rearranged to a list
 			(trans, rot) = listener.lookupTransform('/base_footprint', '/obj_center_pose', rospy.Time(0))
-			#(trans1, rot1) = listener.lookupTransform('/wrist_1_link', '/obj_center_pose', rospy.Time(0))
-			#(trans2, rot2) = listener.lookupTransform('/map', '/obj_center_pose', rospy.Time(0))
-			(trans3, rot3) = listener.lookupTransform('/camera_link', '/base_footprint', rospy.Time(0))
-			#(trans, rot) = listener.lookupTransform('/camera_link', '/camera_color_frame', rospy.Time(0))
 			objToBasePub.publish(listToPose(trans, rot))
-			#objToWrist1Pub.publish(listToPose(trans1, rot1))
-			#objToOriginPub.publish(listToPose(trans2, rot2))
-			camToBasePub.publish(listToPose(trans3, rot3))
-			print "test"
-			print trans3
 		#except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 		#	rospy.loginfo("Warning!")
 		#	continue
