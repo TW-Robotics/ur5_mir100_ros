d533e9d53433e16b69b82c98dfd0b163c0645a0c
Johannes Rauer <johannes.rauer@hotmail.com>
2019-01-11 10:47:43 +0100

    Deleted limited as robot would not move otherwise. No negative effects discovered

--- butler/launch/run_real_butler.bash
+++ butler/launch/run_real_butler.bash
@@ -6,11 +6,11 @@ xterm -hold -e "rosrun butler camera_py2ros_wrapper" &
 xterm -hold -e "roslaunch butler img_rotate.launch" &
 xterm -hold -e "roslaunch butler yolonet.launch" &
 sleep 3
-xterm -hold -e "roslaunch butler butler_robot.launch limited:=true" &
+xterm -hold -e "roslaunch butler butler_robot.launch" &
 sleep 2
 xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 2
-xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true" &
+xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch" &
 sleep 2
 xterm -hold -e "roslaunch ur5_moveit_config moveit_rviz.launch config:=true" &
 sleep 3
