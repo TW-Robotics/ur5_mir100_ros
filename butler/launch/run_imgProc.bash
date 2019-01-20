78a7e39a97817a26ae6317eca7e08673e5911df1
JRauer <johannes.rauer@hotmail.com>
2019-01-20 14:48:44 +0100

    Added comments and cleaned up files

--- butler/launch/run_imgProc.bash
+++ butler/launch/run_imgProc.bash
@@ -1,16 +1,12 @@
 #!/bin/bash
-echo "Running ROS stuff..."
-#echo "Starting roscore"
+
+# Skript to launch the camera-node and yolonet
+echo "Starting roscore"
 xterm -hold -e "roscore" &
 sleep 3
-#xterm -hold -e "roslaunch src/yolonet/usbcam.launch" &
-#xterm -hold -e "roslaunch butler bringUp_camera.launch" &
 xterm -hold -e "rosrun butler camera_py2ros_wrapper" &
 xterm -hold -e "roslaunch butler img_rotate.launch" &
 xterm -hold -e "roslaunch butler yolonet.launch" &
-#xterm -hold -e "roslaunch butler tf_transform.launch" &
-xterm -geometry 96x24+0-0 -hold -e "rosrun butler img_processing cup " &	# Arguments: InnerClass, OuterClass, Strictness (outside, touching (means center of inner box is in outer box), inside)
-#xterm -geometry 96x24+0-0 -hold -e "rosrun butler robot_control" &
 #--------------------#
 #--- We are done! ---#
 #--------------------#
--- butler/launch/run_real_butler.bash
+++ butler/launch/run_real_butler.bash
@@ -1,24 +1,35 @@
 #!/bin/bash
+
+# Skript to launch the complete butler-robot
+echo "Starting roscore..."
 xterm -hold -e "roscore" &
 sleep 3
-#xterm -hold -e "roslaunch realsense2_camera demo_pointcloud.launch" &
+
+echo "Launching basic nodes..."
+echo "Launching camera and neural network..."
 xterm -hold -e "rosrun butler camera_py2ros_wrapper" &
 xterm -hold -e "roslaunch butler img_rotate.launch" &
 xterm -hold -e "roslaunch butler yolonet.launch" &
 sleep 3
+
+echo "Uploading robot-model to core..."
 xterm -hold -e "roslaunch butler butler_robot.launch" &
 sleep 2
+
+echo "Connecting to UR5 and launching path-planner..."
 xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 2
 xterm -hold -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch" &
 sleep 2
 xterm -hold -e "roslaunch ur5_moveit_config moveit_rviz.launch config:=true" &
 sleep 3
+
+echo "Connecting to MiR100..."
 xterm -hold -e "roslaunch mir_driver mir.launch" &
 sleep 15
+
+echo "Launching nodes for tf-transformation..."
 xterm -hold -e "roslaunch butler tf_transform.launch" &
-#--------------------#
-#--- We are done! ---#
-#--------------------#
-echo "Wait for processes ...."
+
+echo "We are done. You can now rosrun butler robot_control."
 wait
