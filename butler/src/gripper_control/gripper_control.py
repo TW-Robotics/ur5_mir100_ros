b2405ad80550ee8c948fd6b85b600f461882a434
JRauer <johannes.rauer@hotmail.com>
2019-01-22 10:07:28 +0100

    Comments and cleanup

--- butler/src/gripper_control/gripper_control.py
+++ butler/src/gripper_control/gripper_control.py
@@ -5,6 +5,8 @@ import rospy
 
 from ur_msgs.msg import IOStates
 
+''' Connects to the gripper via the UR to open/close it and check if grasped successfully'''
+
 HOST = "192.168.12.52"    	# The remote host
 PORT = 30002              	# The same port as used by the server
 
@@ -13,29 +15,32 @@ class gripper(object):
 		rospy.Subscriber('/ur_driver/io_states', IOStates, self.iostatesCallback)
 		self.ioStates = IOStates()
 
+	# Get IO-states to check if grasp has been successful
 	def iostatesCallback(self, data):
 		self.ioStates = data
-		#print ioStates.digital_out_states[4].state
 
+	# Program on UR5 sets DO4 = True if grasped successfully
 	def hasGripped(self):
 		if self.ioStates.digital_out_states[4].state == True:
 			return True
 		else:
 			return False
 
+	# Send command to open the gripper (DO2)
 	def open(self):
 		print "Opening Gripper"
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((HOST, PORT))
 		s.send ("set_digital_out(2,True)" + "\n")
-		print "Open-Gripper Command sent"
+		print "Open-Gripper command sent."
 
+	# Send command to close the gripper (DO3)
 	def close(self):
 		print "Closing Gripper"
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((HOST, PORT))
 		s.send ("set_digital_out(3,True)" + "\n")
-		print "Close-Gripper Command sent"
+		print "Close-Gripper command sent."
 
 def main(args):
 	print "Unable to use package this way!"
